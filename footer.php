a729b4ff638ab62bc0d3ec58732c8643e77715b9
linmi <linmi@linmi.cc>
2018-08-20 01:46:55 +0800

    add: 添加第一个版本

---
+++ footer.php
@@ -0,0 +1,14 @@
+        </div><!-- .site-content -->
+
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="site-info container">
+                <a href="http://www.miitbeian.gov.cn/" target="_blank">皖ICP备15002807号-9 </a>
+            </div>
+        </footer>
+
+    </div><!-- site-inner -->
+</div><!-- site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
